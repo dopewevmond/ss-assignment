69dd99e820153918e1f9746f6333bd2c8e70e497
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-04-20 09:59:17 +0000

    correct database schema

--- api/src/services/db.ts
+++ api/src/services/db.ts
@@ -10,8 +10,8 @@ export const login = async (
   password: string
 ): Promise<Pick<User, 'id' | 'role' | 'firstname' | 'email'>> => {
   const query: QueryConfig = {
-    // text: 'SELECT id, role, firstname, email, password_hash FROM test.users WHERE email = $1 LIMIT 1',
-    text: 'SELECT id, role, firstname, email, password_hash FROM users WHERE email = $1 LIMIT 1',
+    text: 'SELECT id, role, firstname, email, password_hash FROM test.users WHERE email = $1 LIMIT 1',
+    // text: 'SELECT id, role, firstname, email, password_hash FROM users WHERE email = $1 LIMIT 1',
     values: [email]
   }
   const { rows } = await datasource.query<User>(query)
@@ -40,8 +40,8 @@ export const signup = async (
   email: string, password: string, firstname: string, lastname: string, phonenumber: string, gender: string
 ): Promise<Pick<User, 'id' | 'firstname' | 'role' | 'email'>> => {
   const findExistingEmailQuery: QueryConfig = {
-    // text: 'SELECT * FROM test.users WHERE email = $1 OR phonenumber = $2',
-    text: 'SELECT * FROM users WHERE email = $1 OR phonenumber = $2',
+    text: 'SELECT * FROM test.users WHERE email = $1 OR phonenumber = $2',
+    // text: 'SELECT * FROM users WHERE email = $1 OR phonenumber = $2',
     values: [email, phonenumber]
   }
   const { rows: existingEmails } = await datasource.query<User>(findExistingEmailQuery)
@@ -51,14 +51,14 @@ export const signup = async (
   const role: Role = 'customer'
   const id = uuidv4()
   const createNewUserQuery: QueryConfig = {
-    // text: 'INSERT INTO test.users (id, firstname, lastname, phonenumber, email, gender, password_hash, role, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, CURRENT_TIMESTAMP)',
-    text: 'INSERT INTO users (id, firstname, lastname, phonenumber, email, gender, password_hash, role, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, CURRENT_TIMESTAMP)',
+    text: 'INSERT INTO test.users (id, firstname, lastname, phonenumber, email, gender, password_hash, role, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, CURRENT_TIMESTAMP)',
+    // text: 'INSERT INTO users (id, firstname, lastname, phonenumber, email, gender, password_hash, role, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, CURRENT_TIMESTAMP)',
     values: [id, firstname, lastname, phonenumber, email, gender, await hash(password, 10), role]
   }
   await datasource.query<User>(createNewUserQuery)
   const query: QueryConfig = {
-    // text: 'SELECT id, firstname, role, email FROM test.users WHERE id = $1',
-    text: 'SELECT id, firstname, role, email FROM users WHERE id = $1',
+    text: 'SELECT id, firstname, role, email FROM test.users WHERE id = $1',
+    // text: 'SELECT id, firstname, role, email FROM users WHERE id = $1',
     values: [id]
   }
   const { rows } = await datasource.query<User>(query)
@@ -75,8 +75,8 @@ export const getUserDetails = async (
 Pick<User, 'firstname' | 'lastname' | 'phonenumber' | 'email' | 'gender'>
 > => {
   const query: QueryConfig = {
-    // text: 'SELECT firstname, lastname, phonenumber, email, gender FROM test.users WHERE id = $1 LIMIT 1',
-    text: 'SELECT firstname, lastname, phonenumber, email, gender FROM users WHERE id = $1 LIMIT 1',
+    text: 'SELECT firstname, lastname, phonenumber, email, gender FROM test.users WHERE id = $1 LIMIT 1',
+    // text: 'SELECT firstname, lastname, phonenumber, email, gender FROM users WHERE id = $1 LIMIT 1',
     values: [id]
   }
   const { rows } = await datasource.query<User>(query)
@@ -91,16 +91,16 @@ export const updateUserRecord = async (
   { id, firstname, lastname, phonenumber, email, gender }: Pick<User, 'id' | 'firstname' | 'lastname' | 'phonenumber' | 'email' | 'gender'>
 ): Promise<Pick<User, 'firstname' | 'lastname' | 'phonenumber' | 'email' | 'gender'>> => {
   const updateQuery: QueryConfig = {
-    // text: 'UPDATE test.users SET firstname = $1, lastname = $2, phonenumber = $3, email = $4, gender = $5 WHERE id = $6',
-    text: 'UPDATE users SET firstname = $1, lastname = $2, phonenumber = $3, email = $4, gender = $5 WHERE id = $6',
+    text: 'UPDATE test.users SET firstname = $1, lastname = $2, phonenumber = $3, email = $4, gender = $5 WHERE id = $6',
+    // text: 'UPDATE users SET firstname = $1, lastname = $2, phonenumber = $3, email = $4, gender = $5 WHERE id = $6',
     values: [firstname, lastname, phonenumber, email, gender, id]
   }
   await datasource.query<User>(
     updateQuery
   )
   const getUpdatedDetailsQuery: QueryConfig = {
-    // text: 'SELECT firstname, lastname, phonenumber, email, gender FROM test.users WHERE id = $1 LIMIT 1',
-    text: 'SELECT firstname, lastname, phonenumber, email, gender FROM users WHERE id = $1 LIMIT 1',
+    text: 'SELECT firstname, lastname, phonenumber, email, gender FROM test.users WHERE id = $1 LIMIT 1',
+    // text: 'SELECT firstname, lastname, phonenumber, email, gender FROM users WHERE id = $1 LIMIT 1',
     values: [id]
   }
   const { rows } =
