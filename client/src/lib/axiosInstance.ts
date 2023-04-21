d22a118333a71072c1dd2d8709e8b93a6006a9e7
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-04-21 02:43:34 +0000

    changed base url

--- client/src/lib/axiosInstance.ts
+++ client/src/lib/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3001',
-  // baseURL: 'https://pacific-legend-383801.ew.r.appspot.com',
+  baseURL: 'https://pacific-legend-383801.ew.r.appspot.com',
   headers: {
     "Content-Type": "application/json",
   },
